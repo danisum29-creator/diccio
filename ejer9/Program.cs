15e27b5062ee43ae42537c56e72fa66005ddbad0
Daniel S. <edvindaniel427@gmail.com>
2026-03-08 01:18:20 -0600

    ingreso notas

---
+++ ejer9/Program.cs
@@ -0,0 +1,21 @@
+﻿Dictionary<string , double>notas =  new Dictionary<string , double>();
+
+
+string estudiante;
+int op=0; 
+Console.WriteLine("ingrese el nombre del estudiante");
+estudiante = Console.ReadLine();
+Console.WriteLine("ingrese nota del estudiante");
+
+
+do
+{
+    Console.WriteLine("desea ingresar otro estudiante?\n1.si\n2.no");
+    op = int.Parse(Console.ReadLine());
+    switch(op)
+    {
+        case 1:
+
+            break; 
+    }
+}
