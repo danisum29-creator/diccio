64a8a99ecef5143539bbad84ea02950cce24da86
Daniel S. <edvindaniel427@gmail.com>
2026-03-08 01:01:39 -0600

    final ej7 terminado

--- ejer7/Program.cs
+++ ejer7/Program.cs
@@ -1,8 +1,34 @@
 ﻿Dictionary<int , string>  productos =  new Dictionary<int , string>();
 
 int codigo = 1;
+int op = 0;
 string producto; 
 Console.WriteLine("ingrese un producto "); 
 producto =  Console.ReadLine();
 productos.Add(codigo, producto);
+
+do
+{
+    Console.WriteLine("desea ingresar otro producto?\n1. si\n2. no");
+    op = int.Parse(Console.ReadLine());
+    switch(op)
+    {
+        case 1:
+            Console.WriteLine("ingrese un producto ");
+            producto = Console.ReadLine();
+            codigo = codigo + 1;
+
+            productos.Add(codigo, producto);
+
+            
+            break;
+        case 2:
+            break;
+
+        default:
+            Console.WriteLine("error, opcion no valida");
+            break;
+
+    }
+}while (op != 2);
 
