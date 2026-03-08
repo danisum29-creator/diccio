7510057894a037b126763671916927ca49d8eb50
Daniel S. <edvindaniel427@gmail.com>
2026-03-07 23:45:34 -0600

    agregando cambios ingreso

---
+++ ejer5/Program.cs
@@ -0,0 +1,24 @@
+﻿Dictionary<string, string>usuarios = new Dictionary<string, string>();
+
+string clave,usuario;
+int ver = 0; 
+
+do
+{
+    Console.WriteLine("ingrese su usuario ");
+    usuario = Console.ReadLine();
+    Console.WriteLine("ingrese su clave ");
+    clave = Console.ReadLine();
+
+    usuarios.Add(usuario, clave);
+    Console.WriteLine("desea ingresar otro usuario ?\n1. si\n2.no");
+    ver = int.Parse(Console.ReadLine());
+    if( ver <=0 || ver >=3)
+    {
+        Console.WriteLine("error");
+    }
+}while(ver != 2 );
+
+Console.WriteLine("ingrese un usuario");
+usuario = Console.ReadLine();
+Console.WriteLine(usuarios[usuario]);
