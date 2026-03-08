5afa260d16a9cb44502cecf565ce5aba7ba438ca
Daniel S. <edvindaniel427@gmail.com>
2026-03-08 01:45:50 -0600

    finn

--- ejer10/Program.cs
+++ ejer10/Program.cs
@@ -5,6 +5,11 @@ productos.Add(2, "uva");
 productos.Add(3, "manzana");
 productos.Add(4, "melon");
 
+foreach (var item in productos)
+{
+    Console.WriteLine(item);
+}
+
 Console.WriteLine("ingrese el codigo del producto que desea eliminar ");
 codigo = int.Parse(Console.ReadLine());
 
@@ -12,6 +17,13 @@ if(productos.ContainsKey(codigo))
 {
     productos.Remove(codigo);
     Console.WriteLine("el inventario actualizado es: ");
-
+foreach(var item in productos)
+    {
+        Console.WriteLine(item);
+    }
+}
+else
+{
+    Console.WriteLine("el codigo de producto no existe ");
 }
 
