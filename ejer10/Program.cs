18924777f1bf1c082395b9a69a511ad0c74b2538
Daniel S. <edvindaniel427@gmail.com>
2026-03-08 01:42:47 -0600

    ingreso codigo

---
+++ ejer10/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<int,string>productos = new Dictionary<int,string>();
+int codigo = 0; 
+productos.Add(1, "pera");
+productos.Add(2, "uva");
+productos.Add(3, "manzana");
+productos.Add(4, "melon");
+
+Console.WriteLine("ingrese el codigo del producto que desea eliminar ");
+codigo = int.Parse(Console.ReadLine());
+
+if(productos.ContainsKey(codigo))
+{
+    productos.Remove(codigo);
+    Console.WriteLine("el inventario actualizado es: ");
+
+}
+
