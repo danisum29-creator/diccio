ebd42cf4932db36b462f17f7de957dd0947742d6
Daniel S. <edvindaniel427@gmail.com>
2026-03-08 01:06:28 -0600

    terminado

---
+++ ejer8/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<int , string> empleados  = new Dictionary<int , string>();
+
+empleados.Add(1, "raul");
+empleados.Add(2, "joselu");
+empleados.Add(3, "rodrigo");
+
+empleados[3] = "daniel";
+
+foreach (var item in empleados)
+{
+    Console.WriteLine(item);
+}
