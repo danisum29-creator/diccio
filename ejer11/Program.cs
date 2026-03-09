29bbb38f2e194992c5e6eac3037e2a6abe8781ef
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 15:03:01 -0600

    ej11

---
+++ ejer11/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<int , string> estudiantes =  new Dictionary<int , string>();
+
+estudiantes.Add(1, "daniel");
+estudiantes.Add(2, "danna");
+estudiantes.Add(3, "julio");
+estudiantes.Add(4, "rosario");
+estudiantes.Add(5, "ramiro");
+
+foreach (var item in estudiantes)
+{
+    Console.WriteLine(item.Key +" " +item.Value);
+}
