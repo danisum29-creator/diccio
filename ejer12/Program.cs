addbdf493b48f47ae8a4424cc2ad096061fdea1e
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 15:38:50 -0600

    ej12

---
+++ ejer12/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<int , string> productos = new Dictionary<int , string>();
+
+productos.Add(1, "computadora");
+productos.Add(2, "mouse");
+productos.Add(3, "procesador");
+productos.Add(4, "memorias");
+productos.Add(5, "teclados");
+
+foreach (var product in productos)
+{
+    Console.WriteLine(product.Key +" " +product.Value);
+}
---
+++ ejer13/Program.cs
@@ -0,0 +1,9 @@
+﻿Dictionary<string, double> notas = new Dictionary<string, double>();
+
+notas.Add("fisica 3", 80);
+notas.Add("programacion 3", 76);
+notas.Add("circuitos electricos ", 67);
+notas.Add("matematicas", 93);
+notas.Add("proceso economico 1 ", 81);
+notas.Add("matematica 4", 76);
+
