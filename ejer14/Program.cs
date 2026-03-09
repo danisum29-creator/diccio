8db0153aea950d20847cfe611e843145b655c1bb
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 16:13:00 -0600

    ciudad/codigo impresos

--- ejer14/Program.cs
+++ ejer14/Program.cs
@@ -4,24 +4,43 @@ int codigos=0, op;
 
 Console.WriteLine("ingrese una ciudad ");
 ciudad = Console.ReadLine();
+codigos++;
+ciudades.Add(codigos,ciudad);
+
 do
 
 {
-    Console.WriteLine("desea ingresar otra ciudad?  \n1. si\n2. no "); 
-   op = int.Parse(Console.ReadLine());
+    Console.WriteLine("desea ingresar otra ciudad?  \n1. si\n2. no ");
+    op = int.Parse(Console.ReadLine());
 
-    switch(op)
+    switch (op)
     {
         case 1:
-
+            Console.WriteLine("ingrese una ciudad ");
+            ciudad = Console.ReadLine();
+            codigos++;
+            ciudades.Add(codigos, ciudad);
             break;
 
         case 2:
 
             break;
 
-            default: break;
+        default:
+            Console.WriteLine("error, opcion no valida");
+            break;
     }
 
-    
+
+} while (op != 2);
+Console.WriteLine("los codigos de las ciudades son: ");
+
+foreach (var codigo in ciudades)
+{
+    Console.WriteLine(codigo.Key);
+}
+Console.WriteLine("las ciudades son: ");
+foreach(var cid in ciudades)
+{
+    Console.WriteLine(cid.Value);
 }
