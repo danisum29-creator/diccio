d56917b8b882e219f1ac8302853f0434507c8e2f
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 15:44:13 -0600

    notas agregadas promedio calculado

--- ejer13/Program.cs
+++ ejer13/Program.cs
@@ -1,9 +1,22 @@
 ﻿Dictionary<string, double> notas = new Dictionary<string, double>();
-
+double suma = 0, promedio = 0;
+int contnotas = 0; 
 notas.Add("fisica 3", 80);
 notas.Add("programacion 3", 76);
 notas.Add("circuitos electricos ", 67);
 notas.Add("matematicas", 93);
 notas.Add("proceso economico 1 ", 81);
 notas.Add("matematica 4", 76);
+foreach(var i in notas)
+{
+    Console.WriteLine(i);
+}
 
+foreach (var item in notas)
+{
+   suma = suma+item.Value;
+    contnotas++;
+}
+
+promedio = suma/contnotas; 
+Console.WriteLine("el promedio de las notas es: " +promedio);
