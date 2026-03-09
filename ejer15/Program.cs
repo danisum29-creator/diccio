b2c78c2fdeb337d1d893b8dcd96e7b1d789b2f1e
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 16:56:59 -0600

    agregar empleados

---
+++ ejer15/Program.cs
@@ -0,0 +1,29 @@
+﻿Dictionary<int , string> empleados  = new Dictionary<int , string>();
+string empleado;
+int codigos = 1,op=0;
+Console.WriteLine("ingrese el nombre del empleado/a");
+empleado = Console.ReadLine();
+empleados.Add(codigos, empleado);
+
+do
+{
+    Console.WriteLine("desea ingresar el nombre de otro empleado? \n1. si\n2. no");
+    op = int.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            Console.WriteLine("ingrese el nombre del empleado/a");
+            empleado = Console.ReadLine();
+            empleados.Add(codigos, empleado);
+
+            break;
+
+        case 2:
+            Console.WriteLine("error, opcion no valida");
+            break;
+
+        default:
+            break;
+    }
+} while (op != 2); 
+
