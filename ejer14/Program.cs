a4dfd0ee217156d2c1368067e7996f21eb1a3914
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 15:49:57 -0600

    ingreso ciudades

---
+++ ejer14/Program.cs
@@ -0,0 +1,27 @@
+﻿Dictionary<int, string> ciudades = new Dictionary<int, string>();
+string ciudad;
+int codigos=0, op;
+
+Console.WriteLine("ingrese una ciudad ");
+ciudad = Console.ReadLine();
+do
+
+{
+    Console.WriteLine("desea ingresar otra ciudad?  \n1. si\n2. no "); 
+   op = int.Parse(Console.ReadLine());
+
+    switch(op)
+    {
+        case 1:
+
+            break;
+
+        case 2:
+
+            break;
+
+            default: break;
+    }
+
+    
+}
