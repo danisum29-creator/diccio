10221c7ad425e5f12fa33e8b7130e36a5e9fede5
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 19:22:21 -0600

    ingreso estudiantes

---
+++ ejer20/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<int, (string,int, string)> estudiantes = new Dictionary<int, (string,int, string)>();
+int cont = 0, carne, edad ;
+string nombre, carrera;
+do
+{
+    Console.WriteLine("ingrese el numero de carnet del estudiante ");
+    carne = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese el nombre del estudiante");
+    nombre = Console.ReadLine();
+    Console.WriteLine("ingrese la edad del estudiante: ");
+    edad = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese la carrera del estudiante");
+    carrera = Console.ReadLine();
+    cont++;
+} while (cont < 3);
+
+
