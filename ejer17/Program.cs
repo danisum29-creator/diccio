f63bbd424ab46006a67a9caf582d35a222911e3a
Daniel S. <edvindaniel427@gmail.com>
2026-03-09 18:23:14 -0600

    impresion datos

--- ejer17/Program.cs
+++ ejer17/Program.cs
@@ -1,19 +1,30 @@
-﻿Dictionary<int, string> inventario = new Dictionary<int, string>();
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+Dictionary<int, string> inventario = new Dictionary<int, string>();
 int codigos = 1, opMenu;
 string productos,eliminarproducto;
-bool ver = false;
+bool ver = false, eliminado;
 do
 {
-    Console.WriteLine("seleccione una opcion para el inventario");
+   
+
+Console.WriteLine("seleccione una opcion para el inventario");
     Console.WriteLine("1. agregar producto\n2. eliminar producto\n3. mostrar producto\n4. salir");
     opMenu = int.Parse(Console.ReadLine());
 
     switch (opMenu)
     {
+        
         case 1:
+
+            Console.Clear();
+
+
             Console.WriteLine("ingrese el nombre de un producto");
             productos = Console.ReadLine();
             inventario.Add(codigos, productos);
+            codigos++;
             ver = true;
             break;
 
@@ -28,22 +39,36 @@ do
                 Console.WriteLine("escriba el nombre del producto a eliminar");
                 eliminarproducto = Console.ReadLine();
 
-
-                foreach (var produc in inventario)
+               foreach(var prod in inventario)
                 {
-                    if (inventario.ContainsValue(eliminarproducto)) ;
+                    if(prod.Value==eliminarproducto)
                     {
-                        inventario.Remove(codigos);
+                        inventario.Remove(prod.Key); 
+                       
                     }
                 }
+              
+                 
+                
             }
 
             break;
 
         case 3:
            
-
-            break;
+            if (ver == false || inventario.Count==0)
+            {
+                Console.WriteLine("inventario vacio ");
+            }
+            else
+            {
+                Console.WriteLine("el inventario de productos es: ");
+                foreach(var produc in inventario)
+                {
+                    Console.WriteLine(produc.Key +" " + produc.Value);
+                }
+            }
+                break;
     }
 
 } while (opMenu != 4);
